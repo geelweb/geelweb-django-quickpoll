edb86e0d300cef4046b46918c2a11bebaa750d80
Guillaume Luchet <guillaume@geelweb.org>
2013-01-15 07:56:04 +0100

    remove vote field for choices in admin part

--- src/geelweb/django/quickpoll/admin.py
+++ src/geelweb/django/quickpoll/admin.py
@@ -3,16 +3,29 @@
 # License: MIT
 # vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
+"""
+quickpoll admin
+"""
+
 __author__ = "Guillaume Luchet <guillaume@geelweb.org>"
 
 from django.contrib import admin
 from geelweb.django.quickpoll.models import Poll, Question, Choice, Rule
 
 class ChoiceInline(admin.TabularInline):
+    """
+    Choice inline
+    """
     model = Choice
     extra = 3
+    fieldsets = [
+            (None, {'fields': ['choice', 'rank']})
+            ]
 
 class QuestionAdmin(admin.ModelAdmin):
+    """
+    Question admin
+    """
     fieldsets = [
             (None, {'fields': ['question', 'poll', 'rank']}),
     ]
